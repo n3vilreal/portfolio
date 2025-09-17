4b078cdcd77581e823f8b7f35e1a35fe9c5f833e
n3vilreal <nebil.241423@ncit.edu.np>
2025-09-17 11:38:37 +0545

    deploy

--- src/main.jsx
+++ src/main.jsx
@@ -5,10 +5,10 @@ import App from './App.jsx'
 import { HashRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <React.StrictMode>
     <HashRouter>
         <App />
     </HashRouter>
     
-  </StrictMode>,
+  </React.StrictMode>,
 )
